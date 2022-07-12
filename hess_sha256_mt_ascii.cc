992d89f00a69dda117335f3ae92773695606296d
Oscar Riveros <oscar.riveros@gmail.com>
2022-07-12 17:44:38 -0400

    Update hess_sha256_mt_ascii.cc

--- hess_sha256_mt_ascii.cc
+++ hess_sha256_mt_ascii.cc
@@ -93,9 +93,9 @@ void hess(std::string &hash, const int &n, const int id) {
     auto start = std::chrono::steady_clock::now();
     std::vector<unsigned char> bit(n, ' '), aux;
     while (next_orbit(bit)) {
-        float local, global{std::numeric_limits<float>::max()};
         for (auto i{0}; i < n - 1; i++) {
             for (auto j{i + 1}; j < n; j++) {
+                float local, global{std::numeric_limits<float>::max()};
                 for (auto k{0}; k < n; k++) {
                     aux.assign(bit.begin(), bit.end());
                     mutex.lock();
