c48e1ba24ab36527baa9a5e3c65979e316704b56
Oscar Riveros <oscar.riveros@gmail.com>
2022-07-13 12:07:51 -0400

    Update test_4_8.sh

--- bin/test_4_8.sh
+++ bin/test_4_8.sh
@@ -3,4 +3,4 @@
 # ./hess-sha256-apocalypse 268d21eeefd8688292c74b63e6db7da07888e5071af3a2e04e5c49428d9bfb32 4 8 # $dñ0 (ORD('ñ') == 241) (FAIL! restricted to ascii 32-126)
 ./hess-sha256-apocalypse 4a4071e696341623d78d237f2c7501123461bbe02a4af310a1cc9ec654519958 4 8
 ./hess-sha256-apocalypse edac0fecc79e49bcea803d276ab9a912385254db18bb0834098a6dc73bb9683e 4 8
-./hess_sha256-apocalypse 00e2e1f702d0d8a2b233b1041eda4be80d6dae152ecfaba40d184df525c46862 4 8
+./hess-sha256-apocalypse 00e2e1f702d0d8a2b233b1041eda4be80d6dae152ecfaba40d184df525c46862 4 8
